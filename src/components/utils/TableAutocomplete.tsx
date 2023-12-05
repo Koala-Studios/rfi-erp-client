6b49d964202158098f5f8188753f2103c49c32de
frankev6 <frank.ev6@gmail.com>
2023-12-05 17:38:20 -0500

    proper field sizing batching table

--- src/components/utils/TableAutocomplete.tsx
+++ src/components/utils/TableAutocomplete.tsx
@@ -91,7 +91,7 @@ const TableAutocomplete: React.FC<Props> = ({
             {children}
           </Paper>
         )}
-        sx={{ width: width, height: "39px" }}
+        sx={{ width: width }}
         onInputChange={handleInputChange}
         onBlur={() => setEditMode(false)}
         onChange={(event, value) => {
@@ -100,21 +100,17 @@ const TableAutocomplete: React.FC<Props> = ({
           setEditMode(false);
         }}
         renderInput={(params) => {
-          params.inputProps.style = { textTransform: "capitalize" }; //textTransform uppercase doesn't work on style
           return (
             <TextField
               variant="outlined"
               autoFocus
               style={{
-                width: width,
-                maxWidth: width,
-                // minHeight: "39px",
-                maxHeight: "100%",
                 borderRadius: 0,
+                overflow: "hidden",
               }}
               placeholder={initialValue}
               {...params}
-              InputProps={{ sx: { borderRadius: 0, maxHeight: "100%" } }}
+              InputProps={{ sx: { borderRadius: 0, maxHeight: "45px" } }}
             />
           );
         }}
@@ -132,7 +128,10 @@ const TableAutocomplete: React.FC<Props> = ({
         }}
         onDoubleClick={() => setEditMode(!readOnly)}
       >
-        <Typography variant="subtitle2" style={{ fontSize: "15px" }}>
+        <Typography
+          variant="subtitle2"
+          style={{ fontSize: "15px", cursor: "pointer" }}
+        >
           {initialValue}
         </Typography>
       </div>
--- src/components/utils/TableComponents.tsx
+++ src/components/utils/TableComponents.tsx
@@ -1,62 +1,74 @@
 import { Key } from "@mui/icons-material";
-import { Chip, Menu, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
+import {
+  Chip,
+  Menu,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { GridColDef, useGridApiContext } from "@mui/x-data-grid";
 import React, { useState } from "react";
 
-
-interface SingleDropdownCellProps{
-    id?:any;
-    value?:any;
-    field?:any;
-    options:(string | number)[][];
-    handleEditRow: (rowid:string, newValue: number | string) => void;
-    //value,text
-    // options:{value:number | string, text:string}[];
+interface SingleDropdownCellProps {
+  id?: any;
+  value?: any;
+  field?: any;
+  options: (string | number)[][];
+  handleEditRow: (rowid: string, newValue: number | string) => void;
+  //value,text
+  // options:{value:number | string, text:string}[];
 }
 
-export const SingleDropdownCell: React.FC<SingleDropdownCellProps> = ({id, value, field,options, handleEditRow}) => {
-    const apiRef = useGridApiContext();
-  
-    const handleChange = (newValue: string | number) => {
-      handleEditRow(id, newValue);
-      handleClose()
-    };
-  
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+export const SingleDropdownCell: React.FC<SingleDropdownCellProps> = ({
+  id,
+  value,
+  field,
+  options,
+  handleEditRow,
+}) => {
+  const apiRef = useGridApiContext();
 
+  const handleChange = (newValue: string | number) => {
+    handleEditRow(id, newValue);
+    handleClose();
+  };
 
-    return (
-        <>
-        <Chip
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Chip
         onClick={handleClick}
-          label={options[value-1][1]}
-          sx={{
-            fontWeight: 600,
-          }}
-          //@ts-ignore
-          color={options[value-1][2]}
-          variant="outlined"
-        />
-        <Menu
-         anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
+        label={options[value - 1][1]}
+        sx={{
+          fontWeight: 600,
+        }}
+        //@ts-ignore
+        color={options[value - 1][2]}
+        variant="outlined"
+      />
+      <Menu
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
         anchorEl={anchorEl}
         open={open}
-        onClose={handleClose}        
-        sx={{border:"1px solid #00000015"}}
+        onClose={handleClose}
+        sx={{ border: "1px solid #00000015" }}
         PaperProps={{
           elevation: 0,
           sx: {
@@ -72,83 +84,93 @@ export const SingleDropdownCell: React.FC<SingleDropdownCellProps> = ({id, value
             },
           },
         }}
-        >
-
-            {options.map((item)=>{
-                return(
-                <MenuItem key={item[0]} onClick={() => handleChange(item[0])} sx={{borderBottom:"1px solid #00000020", p:1}}>
-                    <Chip
-                        onClick={handleClick}
-                        label={item[1]}
-                        sx={{
-                            fontWeight: 600,
-                        }}
-                        //@ts-ignore
-                        color={item[2]}
-                        variant="outlined"
-                    />
-                </MenuItem>)
-            })}
-        </Menu>
+      >
+        {options.map((item) => {
+          return (
+            <MenuItem
+              key={item[0]}
+              onClick={() => handleChange(item[0])}
+              sx={{ borderBottom: "1px solid #00000020", p: 1 }}
+            >
+              <Chip
+                onClick={handleClick}
+                label={item[1]}
+                sx={{
+                  fontWeight: 600,
+                }}
+                //@ts-ignore
+                color={item[2]}
+                variant="outlined"
+              />
+            </MenuItem>
+          );
+        })}
+      </Menu>
     </>
+  );
+};
 
-    );
-  }
+export const MultiDropdownCell: React.FC<SingleDropdownCellProps> = ({
+  id,
+  value,
+  field,
+  options,
+  handleEditRow,
+}) => {
+  const apiRef = useGridApiContext();
 
-export const MultiDropdownCell: React.FC<SingleDropdownCellProps> = ({id, value, field,options, handleEditRow}) => {
-    const apiRef = useGridApiContext();
-  
-    const handleChange = (newValue: string | number) => {
-      handleEditRow(id, newValue);
-      handleClose()
-    };
-  
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+  const handleChange = (newValue: string | number) => {
+    handleEditRow(id, newValue);
+    handleClose();
+  };
 
-    console.log(value);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <>
-        <div onClick={handleClick} style={{display:"flex", width:"100%", flexWrap:"wrap", gap:5}}>
-          
-      {value.map((val:number)=>{
+  console.log(value);
 
-        return (<Chip
-        
-          label={options[val-1][1]}
-          sx={{
-            m:0,
-            fontWeight: 600,
-          }}
-          //@ts-ignore
-          color={options[val-1][2]}
-          variant="outlined"
-          size="small"
-        />)
-      })}
-        
-        </div>
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        style={{ display: "flex", width: "100%", flexWrap: "wrap", gap: 5 }}
+      >
+        {value.map((val: number) => {
+          return (
+            <Chip
+              label={options[val - 1][1]}
+              sx={{
+                m: 0,
+                fontWeight: 600,
+              }}
+              //@ts-ignore
+              color={options[val - 1][2]}
+              variant="outlined"
+              size="small"
+            />
+          );
+        })}
+      </div>
 
-        <Menu
-         anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
+      <Menu
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
         anchorEl={anchorEl}
         open={open}
-        onClose={handleClose}        
-        sx={{border:"1px solid #00000015"}}
+        onClose={handleClose}
+        sx={{ border: "1px solid #00000015" }}
         PaperProps={{
           elevation: 0,
           sx: {
@@ -164,99 +186,98 @@ export const MultiDropdownCell: React.FC<SingleDropdownCellProps> = ({id, value,
             },
           },
         }}
-        >
-
-            {options.map((item)=>{
-                return(
-                <MenuItem key={item[0]} onClick={() => handleChange(item[0])} sx={{borderBottom:"1px solid #00000020", p:1}}>
-                    <Chip
-                        onClick={handleClick}
-                        label={item[1]}
-                        sx={{
-                            fontWeight: 600,
-                        }}
-                        //@ts-ignore
-                        color={item[2]}
-                        variant="outlined"
-                    />
-                </MenuItem>)
-            })}
-        </Menu>
+      >
+        {options.map((item) => {
+          return (
+            <MenuItem
+              key={item[0]}
+              onClick={() => handleChange(item[0])}
+              sx={{ borderBottom: "1px solid #00000020", p: 1 }}
+            >
+              <Chip
+                onClick={handleClick}
+                label={item[1]}
+                sx={{
+                  fontWeight: 600,
+                }}
+                //@ts-ignore
+                color={item[2]}
+                variant="outlined"
+              />
+            </MenuItem>
+          );
+        })}
+      </Menu>
     </>
-
-    );
-  }
-
+  );
+};
 
 interface TableTexfieldProps {
-    handleEditRow: (value:string) => void;
-    width?: number;
-    readOnly?:boolean;
-    initialValue:string;
-    type?: string;
+  handleEditRow: (value: string) => void;
+  width?: number;
+  readOnly?: boolean;
+  initialValue: string;
+  type?: string;
 }
 
 export const TableTexfield: React.FC<TableTexfieldProps> = ({
-    handleEditRow,
-    initialValue,
-    width = 150,
-    type,
-    readOnly = false
-  }) => {
-    const [editMode, setEditMode] = React.useState(false);
-    const [defaultValue, setDefaultValue] = React.useState(initialValue)
-    if (readOnly == false && editMode) {
-        return (
-            <TextField
-              variant="outlined"
-              autoFocus
-              type={type ? type : "text"}
-              onChange={(event) => setDefaultValue(event.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                handleEditRow(defaultValue);
-                setEditMode(false)
-                }
-                if(e.key === "Escape") {
-                  setDefaultValue(initialValue)
-                  setEditMode(false);
-                };
-              }}
-              //TODO: fix can't make field empty
-              onBlur={(event)=> {
-                console.log(event, 'test if empty', event.target.value)
-                handleEditRow(defaultValue);
-                setEditMode(false)
-              }}
-              InputProps={{ sx: { borderRadius: 0 } }}
-              sx={{
-                width:width,
-                maxWidth:width,
-                // marginLeft: "-5%",
-                // minWidth: "110%",
-                // minHeight: "39px",
-                margin:"0!important",
-              }}
-              value={defaultValue}
-              // placeholder={'-'}
-            //   {...params}
-            />
-          )
-    } else {
-      return (
-        <div
-          style={{
-            width:width,
-            maxWidth:width,
-            padding: "16px",
-          }}
-          onClick={() => setEditMode(!readOnly)}
-        >
-          <Typography variant="subtitle2" style={{ fontSize: "15px" }}>
-            {initialValue}
-          </Typography>
-        </div>
-      );
-    }
-  };
-  
+  handleEditRow,
+  initialValue,
+  width = 150,
+  type,
+  readOnly = false,
+}) => {
+  const [editMode, setEditMode] = React.useState(false);
+  const [defaultValue, setDefaultValue] = React.useState(initialValue);
+  if (readOnly == false && editMode) {
+    return (
+      <TextField
+        variant="outlined"
+        autoFocus
+        type={type ? type : "text"}
+        onChange={(event) => setDefaultValue(event.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleEditRow(defaultValue);
+            setEditMode(false);
+          }
+          if (e.key === "Escape") {
+            setDefaultValue(initialValue);
+            setEditMode(false);
+          }
+        }}
+        //TODO: fix can't make field empty
+        onBlur={(event) => {
+          console.log(event, "test if empty", event.target.value);
+          handleEditRow(defaultValue);
+          setEditMode(false);
+        }}
+        InputProps={{
+          sx: { borderRadius: 0, maxHeight: "45px" },
+        }}
+        sx={{
+          overflow: "hidden",
+        }}
+        value={defaultValue}
+        // placeholder={'-'}
+        //   {...params}
+      />
+    );
+  } else {
+    return (
+      <div
+        style={{
+          width: width,
+          maxWidth: width,
+          padding: "16px",
+          cursor: "pointer",
+        }}
+        onClick={() => setEditMode(!readOnly)}
+      >
+        <Typography variant="subtitle2" style={{ fontSize: "15px" }}>
+          {initialValue}
+        </Typography>
+      </div>
+    );
+  }
+};
